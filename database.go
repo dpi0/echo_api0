6775525346aa00cba4318344a127abd2f63a9ef4
dpi0 <dpi0.dev@proton.me>
2023-10-30 00:37:14 +0530

    fixed dotenv file bug

--- database.go
+++ database.go
@@ -2,39 +2,17 @@ package main
 
 import (
 	"fmt"
-	"context"
-	"os"
-	"time"
-
-	// "go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
-
+    // "context"
 )
 
+func UseClientFromHelper() {
+    // You can use 'client' from main.go here
+    // ctx := context.TODO()
 
-func DBInit(){
-	var mongoUri = os.Getenv("ATLAS_URI")
-	
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-	
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI(
-		mongoUri,
-	))
-	
-	defer func() {
-		if err = client.Disconnect(ctx); err != nil {
-			panic(err)
-		}
-		}()
-		
-		err = client.Ping(ctx, nil)
-		
-		if err != nil {
-		fmt.Println("There was a problem connecting to your Atlas cluster. Check that the URI includes a valid username and password, and that your IP address has been added to the access list. Error: ")
-		panic(err)
-	}
+    // Example usage: Performing an operation using 'client'
+    // collection := client.Database("exampleDB").Collection("exampleCollection")
+    // Use 'collection' or perform operations using 'client' as needed
 
-	fmt.Println("Connected to MongoDB! ✅")
+	fmt.Println("bruhhhhhhhh")
+	// fmt.Println(client)
 }
--- server.go
+++ server.go
@@ -5,16 +5,63 @@ import (
 	"net/http"
 	// "log"
 	"os"
+	"context"
+	"time"
+	"log"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 
+	// "go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+
+	"github.com/joho/godotenv"
+
 )
 
-func main(){
-	e := echo.New()
+func init() {
+    err := godotenv.Load(".env")
+    if err != nil {
+        log.Fatal("Error loading .env file")
+    }
+}
 
-	DBInit()
+
+var client *mongo.Client // Declare client at the package level
+
+
+func main(){
+    mongoUri := os.Getenv("ATLAS_URI")
+
+    ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+    defer cancel()
+
+    var err error
+    client, err = mongo.Connect(ctx, options.Client().ApplyURI(mongoUri))
+
+    if err != nil {
+        panic(err)
+    }
+
+    // Defer the disconnection
+    defer func() {
+        if err = client.Disconnect(ctx); err != nil {
+            panic(err)
+        }
+    }()
+
+    err = client.Ping(ctx, nil)
+
+    if err != nil {
+        fmt.Println("There was a problem connecting to your Atlas cluster. Check that the URI includes a valid username and password, and that your IP address has been added to the access list. Error: ")
+        panic(err)
+    }
+
+    fmt.Println("Connected to MongoDB! ✅")
+
+	UseClientFromHelper()
+	e := echo.New()
 
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
@@ -32,6 +79,4 @@ func hello(c echo.Context) error {
 		"key2": "value2",
 	}
 	return c.JSONPretty(http.StatusOK, data, "  ")
-
 }
-
